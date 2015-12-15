439a351959c557d7780f13aa2a5ed2cc946e6267
Able <Able@10.24.72.245>
2015-12-15 12:18:35 -0500

    Commit with comments and logic explanation in comments

--- src/ExamHearRateCalculator/Main.java
+++ src/ExamHearRateCalculator/Main.java
@@ -2,6 +2,22 @@
 //Student No: 300 806 936
 //Program description: Program to Calculate the maximum heart rate and target heart rate of a person when he/she inputs their age.
 
+/* * #############################LOGIC FOR THE ENTIRE APPLICATION& ###################################33
+ * 
+ * int Age = 40;
+ * 
+ * maximumHeartRate = 220 - 40;
+ * 
+ * targetLowerRangeOfHeartRate = 0.50*maximumHeartRate;
+ * 
+ * targetUpperRangeOfHeartRate = 0.85*maximumHeartRate;
+ * 
+ * System.out.println('maximum heart rate '+ maximumHeartRate);
+ * System.out.println('lower target heart range '+ targetLowerRangeOfHeartRate);
+ * System.out.println('upper target heart range '+ targetUpperRangeOfHeartRate);
+ * 
+ * */
+//********************************************************************************************
 package ExamHearRateCalculator;
 	
 import javafx.application.Application;
